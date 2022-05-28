8847c6cc2439e77b48cc69393c85c11992187590
AlexisBogroff <alexis.bogroff@gmail.com>
2022-05-28 09:58:52 +0200

    Add project (not finished)

--- src/create_exercises_complete_folder.sh
+++ src/create_exercises_complete_folder.sh
@@ -6,11 +6,14 @@ mkdir ../build/export/exercises
 mkdir ../build/export/correction
 mkdir ../build/export/exercises/examples
 mkdir ../build/export/correction/examples
+mkdir ../build/export/project
 
+cp intro_python_exercise_correction.ipynb ../build/export/correction/intro_python_exercise_correction.ipynb
+# TODO: add the version without correction
 cp truc.py ../build/export/exercises/truc.py
 cp truc.py ../build/export/correction/truc.py
-
 cp examples/TD6_Exam_MCQ.csv ../build/export/exercises/examples/TD6_Exam_MCQ.csv
 cp examples/TD6_Exam_MCQ.csv ../build/export/correction/examples/TD6_Exam_MCQ.csv
 
-cp intro_python_exercise_correction.ipynb ../build/export/correction/intro_python_exercise_correction.ipynb
+cp ../build/Project.pdf ../build/export/project/Project.pdf
+cp masse-salariale-et-assiette-chomage-partiel-mensuelles-du-secteur-prive.csv ../build/export/project/masse-salariale-et-assiette-chomage-partiel-mensuelles-du-secteur-prive.csv
